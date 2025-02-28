ef21a257cbf51e583335033cd50b3699afe20431
dmwang1 <112016926+dmwang1@users.noreply.github.com>
2025-02-28 11:44:14 -0500

    final wifi working upload

--- boot.py
+++ boot.py
@@ -7,7 +7,7 @@ import network
 # Set up the WiFi access point with a distinctive name
 ap = network.WLAN(network.AP_IF)
 ap.active(True)
-ap.config(essid='ESP32_TEST_AP', password='micropython', authmode=network.AUTH_WPA_WPA2_PSK)
+ap.config(essid='ESP32_first', password='micropython', authmode=network.AUTH_WPA_WPA2_PSK)
 print("Access Point configured. Connect to WiFi network ESP32_TEST_AP")
 print("IP address:", ap.ifconfig()[0])
 
